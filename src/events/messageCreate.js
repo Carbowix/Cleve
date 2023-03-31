2e1c35a8a2cd67e285ed9b3915eede23e994d1d6
Carbowix <killeryoyo01@gmail.com>
2023-03-31 19:32:51 +0200

    fixed #55

--- src/events/messageCreate.js
+++ src/events/messageCreate.js
@@ -5,9 +5,9 @@ module.exports = {
     once: false,
     run: (client, msg) => {
         if (msg.author.bot) return;
-        if (msg.channel.isDMBased() && !misc.directMessage) return; // Disallow direct messages if configuration says so.
+        if (msg.channel.isDMBased() && !client.misc.directMessage) return; // Disallow direct messages if configuration says so.
         const mentionCheck = (msg.content.startsWith(`<@${msg.client.user.id}>`) || msg.content.startsWith(`<@!${msg.client.user.id}>`));
-        if (mentionCheck || misc.noMention) {
+        if (mentionCheck || client.misc.noMention) {
             handleTalk(client, msg);
         }
     }
